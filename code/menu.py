5d25ed036ef5cb3a24e3de6d31892cf1eebb1691
Ahmad Zafar <amz9616@gmail.com>
2023-09-29 20:28:39 -0400

    menu

--- code/menu.py
+++ code/menu.py
@@ -6,8 +6,52 @@ class Menu:
         # general setup
         self.player = player
         self.toggle_menu = toggle_menu
-        self.display_surface - pygame.display.get_surface()
+        self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font('./font/LycheeSoda.ttf', 30)
 
+        # options
+        self.width = 400
+        self.space = 10
+        self.padding = 8
+
+        # entries
+        self.options = list(self.player.item_inventory.keys()) + list(self.player.seed_inventory.keys())
+        self.sell_border = len(self.player.item_inventory) - 1
+        self.setup()
+
+    def display_money(self):
+        text_surf = self.font.render(f'${self.player.money}', False, 'Black')
+        text_rect = text_surf.get_rect(midbottom = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 20))
+
+        pygame.draw.rect(self.display_surface, 'White', text_rect.inflate(10,10), 0, 4)
+        self.display_surface.blit(text_surf, text_rect)
+
+    def setup(self):
+        self.text_surfs = []
+        self.total_height = 0
+        for item in self.options:
+            text_surf = self.font.render(item, False, 'Black')
+            self.text_surfs.append(text_surf)
+            self.total_height += text_surf.get_height() + (self.padding * 2)
+        
+        self.total_height += (len(self.text_surfs) - 1) * self.space
+        self.menu_top = SCREEN_HEIGHT / 2 - self.total_height / 2
+        self.main_rect = pygame.Rect(SCREEN_WIDTH / 2 - self.width / 2, self.menu_top, self.width, self.total_height)
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_ESCAPE]:
+            self.toggle_menu()
+
+    def show_entry(self, text_surf, amount, top):
+        bg_rect = pygame.Rect(self.main_rect.left, top, self.width, text_surf.get_height() + (self.padding * 2))
+        pygame.draw.rect(self.display_surface, 'White', bg_rect, 0, 4)
+
     def update(self):
-        self.display_surface.blit(pygame.Surface((1000,1000), (0,0)))
+        self.input()
+        self.display_money()
+        for text_index, text_surf in enumerate(self.text_surfs):
+            top = self.main_rect.top + text_index * (text_surf.get_height() + (self.padding * 2) + self.space)
+            self.show_entry(text_surf, 0,  top)
+            self.display_surface.blit(text_surf, (100, text_index * 50))
